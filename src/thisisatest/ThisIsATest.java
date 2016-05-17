49add28a99cb48b6b3995fa7cca2f9b559a98d56
swashyson <Gul_homer@hotmail.com>
2016-05-17 13:52:48 +0200

    #First

---
+++ src/thisisatest/ThisIsATest.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package thisisatest;
+
+/**
+ *
+ * @author gul_h_000
+ */
+public class ThisIsATest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("1");
+    }
+    
+}
